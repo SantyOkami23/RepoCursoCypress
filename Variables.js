d6e016799e0e155df82e0594900d13cc1d3faf95
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2023-06-15 17:12:11 -0500

    Sintaxis variables

---
+++ Variables.js
@@ -0,0 +1,8 @@
+/* Este es un ccmentario de varias lineas */
+let nombre = "Santy";
+let apellido = "Valdez";
+let edad = 21;
+let variableSinTipo  = "Hola";
+
+console.log(nombre, apellido, edad);
+console.log (variableSinTipo+"   " +nombre);
